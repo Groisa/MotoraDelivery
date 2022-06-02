bc6217b9ab9847182a434c2bf13b2c4b52d1e879
Gabriel Eduardo <gabrieleduardo.s.rosa@gmail.com>
2022-06-01 23:15:06 -0300

    implementa firebase

---
+++ front-end/src/Services/firebase.ts
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDF5uZ9i3Y6mgtuWkopLBhMrZfnwBPkJF4",
+  authDomain: "motoradelivery.firebaseapp.com",
+  projectId: "motoradelivery",
+  storageBucket: "motoradelivery.appspot.com",
+  messagingSenderId: "846331802993",
+  appId: "1:846331802993:web:9bc33590c1c0823c2272d0"
+};
+
+
+const app = initializeApp(firebaseConfig);
