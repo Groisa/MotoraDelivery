feee2b47eae5016d5acc3cf104bd766fba159ee2
Gabriel Eduardo <gabrieleduardo.s.rosa@gmail.com>
2022-06-08 21:11:26 -0300

    implementa formik na tela de cadastro motora

--- front-end/src/Components/FormField/index.tsx
+++ front-end/src/Components/FormField/index.tsx
@@ -1,16 +1,28 @@
 import { Form, FormControlProps } from "react-bootstrap"
+import { IMaskInput } from "react-imask"
 
 type Props = {
     controlId: string
     label?: string
     error?: string
+    mask?: { mask: string }[]
+    onAccept?: (value: unknown) =>void
 } & FormControlProps
 
-export function FormFilde({ controlId, label, error, ...inputProps }: Props) {
+export function FormFilde({ controlId, label, error, mask, onAccept , ...inputProps }: Props) {
     return (
         <Form.Group className='mb-3' controlId={controlId}>
             {label && <Form.Label className='mb-1'>{label}</Form.Label>}
-            <Form.Control className="p-2" {...inputProps} />
+            {mask ? (
+                <Form.Control className="p-2"
+                {...inputProps}
+                as={IMaskInput}
+                mask={mask}
+                onAccept={onAccept}
+                />
+            ) :
+                <Form.Control className="p-2" {...inputProps} />
+            }
             <Form.Control.Feedback type='invalid'>
                 {error}
             </Form.Control.Feedback>
--- front-end/src/Views/Register/registerMotora.tsx
+++ front-end/src/Views/Register/registerMotora.tsx
@@ -1,3 +1,4 @@
+import { Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { FormFilde } from "../../Components/FormField";
 import { Layout } from "../../Components/Layout";
@@ -12,50 +13,82 @@ export function MotoraRegister() {
             </ContainerTitleCadastro>
             <ContainerFormCadastro>
                 <h1>Cadastre-se</h1>
-                <FormFilde
-                    controlId="UserNome"
-                    label="Nome"
-                    placeholder="Digite seu nome"
-                // error="Prencha seu nome"
-                // isInvalid
-                />
-                <FormFilde
-                    controlId="UserEmail"
-                    label="Email"
-                    type='email'
-                    placeholder="Digite seu email"
-                // error="Prencha seu nome"
-                // isInvalid
-                />
-                <FormFilde
-                    controlId="PhoneUser"
-                    label="Telefone"
-                    placeholder="Digite seu telefone"
-                    type="number"
-                />
-                <FormFilde
-                    controlId="userCpf"
-                    label="Cpf"
-                    placeholder="Digite seu Cpf"
-                    type="number"
-                />
-                <FormFilde
-                    controlId="UserEndereço"
-                    label="Endereço"
-                    placeholder="Digite seu endereço"
+                <Form>
+                    <FormFilde
+                        controlId="UserNome"
+                        label="Nome"
+                        placeholder="Digite seu nome"
+                    // error="Prencha seu nome"
+                    // isInvalid
+                    />
+                    <FormFilde
+                        controlId="UserEmail"
+                        label="Email"
+                        type='email'
+                        placeholder="Digite seu email"
+                    // error="Prencha seu nome"
+                    // isInvalid
+                    />
+                    <FormFilde
+                        type='password'
+                        controlId="Userpassword"
+                        label="Senha"
+                        placeholder="Insira sua senha"
+                    // error="Prencha seu nome"
+                    // isInvalid
+                    />
+                    <FormFilde
+                        controlId="PhoneUser"
+                        label="Telefone"
+                        placeholder="Digite seu telefone"
+                        mask={[
+                            { mask: '(00) 00000-0000' }
+                        ]}
+                    />
+                    <FormFilde
+                        controlId="userCpf"
+                        label="Cpf"
+                        placeholder="Digite seu Cpf"
+                        mask={[
+                            { mask: '000.000.000-00' }
+                        ]}
+                    />
+                    <FormFilde
+                        controlId="UserEndereço"
+                        label="Endereço"
+                        placeholder="Digite seu endereço"
 
-                />
-                <FormFilde
-                    controlId="UserPlaca"
-                    label="Placa"
-                    placeholder="Digite sua placa"
-                />
-                <FormFilde
-                    controlId="UserModelo"
-                    label="Modelo Veiculo"
-                    placeholder="Digite o modelo do seu Veiculo"
-                />
-                <ButtonFormUser>Cadastrar</ButtonFormUser>
+                    />
+                    <FormFilde
+                        controlId="userData"
+                        label="Data de Nascimento"
+                        placeholder="Insira seu ano de nascimento"
+                        mask={[
+                            { mask: '00/00/0000' }
+                        ]}
+                    />
+                    <FormFilde
+                        controlId="UserPlaca"
+                        label="Placa"
+                        placeholder="Digite sua placa"
+                        mask={[
+                            { mask: 'aaa-0000' },
+                            { mask: 'aaa0a00' },
+                        ]}
+                    />
+                    <FormFilde
+                        controlId="UserModelo"
+                        label="Modelo Veiculo"
+                        placeholder="Digite o modelo do seu Veiculo"
+                    />
+                    <FormFilde
+                        type='file'
+                        controlId="UerFoto"
+                        label="Foto"
+                        placeholder="Insira sua foto"
+                    />
+                    <ButtonFormUser>Cadastrar</ButtonFormUser>
+                </Form>
             </ContainerFormCadastro>
             <ContainerAlterniveUser>
                 <h2>Já possui Cadastro?</h2>
