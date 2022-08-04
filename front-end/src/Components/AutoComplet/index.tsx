eeb78bb57ae7563c9d313d280eede30f782479aa
Gabriel Eduardo <gabrieleduardo.s.rosa@gmail.com>
2022-08-04 19:49:40 -0300

    implementa busca de endereço

--- front-end/src/Components/AutoComplet/index.tsx
+++ front-end/src/Components/AutoComplet/index.tsx
@@ -1,11 +1,22 @@
 import { Autocomplete } from "@react-google-maps/api";
+import { useRef } from "react";
 import { FormFilde, Props } from "../FormField";
 import { LoadGoogleScript } from "../LogingGoogleKey";
 
 export function AutoCompleteField(formProps: Props) {
+    const autoCompleteRef = useRef<null | google.maps.places.Autocomplete>(null)
+    const handleLoad = (autocomplete: google.maps.places.Autocomplete) => {
+        autoCompleteRef.current = autocomplete
+    }
+    const handleChange = () => {
+        console.log(autoCompleteRef.current?.getPlace())
+    }
     return (
         <LoadGoogleScript>
-            <Autocomplete>
+            <Autocomplete
+                onLoad={handleLoad}
+                onPlaceChanged={handleChange}
+            >
                 <FormFilde
                     {...formProps}
                 />
--- front-end/src/Components/LogingGoogleKey/index.tsx
+++ front-end/src/Components/LogingGoogleKey/index.tsx
@@ -6,12 +6,15 @@ type Props = {
 }
 const libraries: ("drawing" | "geometry" | "localContext" | "places" | "visualization")[] = ['places']
 export function LoadGoogleScript( { children }: Props) {
-    const {loadError} = useLoadScript({
+    const {loadError, isLoaded} = useLoadScript({
         googleMapsApiKey:'AIzaSyALlCd2X12quqEOjlH2Dqj98vIMvuWVxAQ',
         libraries
     })
     if (loadError) {
         return <Alert variant="danger">Falha ao carregar o Google. Recarregue a página.</Alert>
+      }
+      if(!isLoaded) {
+        return <Alert variant="danger">Carregando....</Alert>
       }
     return children
 }
